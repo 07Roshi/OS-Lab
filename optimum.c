b86b4c9e2f4c4a1593b0e62782a7618f009c7fd8
Shetty Roshith Raghurama <121103694+07Roshi@users.noreply.github.com>
2023-07-27 06:33:46 +0000

    upadte

---
+++ optimum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int findIndex(int frames[], int n, int page)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(frames[i]==page)
+        return i;
+    }
+    return -1;
+}
+
+void printFrame(int frames[], int n)
+{
+    for (int n=0;i<n;i++)
+    {
+        if(frames[i]==-1)
+        printf(" ");
+        else
+        printf("%d",frames[i]);
+    }
+    printf("\n");
+}
